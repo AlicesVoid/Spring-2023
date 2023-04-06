7712ddd7e6cd93e96f08955bc89954cee6cfa396
Alice Void <hypnocarrot@csu.fullerton.edu>
2023-04-05 19:57:25 -0700

    python project is fully functional

---
+++ COMPILERS/PROJECT_LEXER/lexer.py
@@ -0,0 +1,70 @@
+import re
+import os
+
+class LexerCPP:
+
+    # Constructor
+    def __init__(self, file_path):
+        self.token_types = []
+        self.lexemes = []
+        self.cpp_code = self.read_cpp_code_from_file(file_path)
+        self.tokenize()
+
+    # CPP Text-File Reading Method
+    def read_cpp_code_from_file(self, file_path):
+        with open(file_path, 'r') as file:
+            return file.read()
+
+    # Tokenizes CPP Code
+    def tokenize(self):
+        # Regular expressions for integers, identifiers, operators, and separators
+        integer_regex = r'\b\d+\b'
+        identifier_regex = r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'
+        operator_regex = r'[<>=+\-*/%]'
+        separator_regex = r'[();]'
+
+        # Other tokens in the code, such as keywords
+        other_tokens = {
+            'while': 'KEYWORD',
+        }
+
+        # Iterate over each token in the input code
+        for token in self.cpp_code.split():
+            if re.match(integer_regex, token):
+                self.token_types.append('INTEGER')
+                self.lexemes.append(token)
+            elif re.match(identifier_regex, token):
+                token_type = other_tokens.get(token, 'IDENTIFIER')
+                self.token_types.append(token_type)
+                self.lexemes.append(token)
+            else:
+                for char in token:
+                    if char in other_tokens:
+                        self.token_types.append(other_tokens[char])
+                        self.lexemes.append(char)
+                    elif re.match(operator_regex, char):
+                        self.token_types.append('OPERATOR')
+                        self.lexemes.append(char)
+                    elif re.match(separator_regex, char):
+                        self.token_types.append('SEPARATOR')
+                        self.lexemes.append(char)
+
+    # Prints the Types and Lexemes
+    def write_tokens_to_file(self, output_file='output.txt'):
+        with open(output_file, 'w') as file:
+            file.write("TokenType | Lexeme\n\n")
+            for token_type, lexeme in zip(self.token_types, self.lexemes):
+                file.write(f"{token_type} | {lexeme}\n")
+
+
+# Get the directory of the script and join it with the input file name
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Collects the Input File 
+cpp_path = os.path.join(script_directory, 'input_scode.txt')
+
+# Instantiate the LexerCPP class and tokenize the C++ code
+lexer = LexerCPP(cpp_path)
+
+# Print the token-types and lexemes lists
+lexer.write_tokens_to_file()
---
+++ COMPILERS/PROJECT_LEXER/readme.md
@@ -0,0 +1,24 @@
+# CPSC 323 PROJECT 1: The Lexer #   
+> Author: Amelia Rotondo   
+> Last Modified: April 6th, 2023    
+
+# File Reading/Writing:  
+1. The Lexer reads a single text file named `input_scode.txt`
+2. The Lexer writes a single text file named `output.txt`
+  
+# Input Format:  
+This lexer is abridged, and does not include all C++ Methods for concision purposes. The methods supported by this lexer are:   
+- All Integers  
+- All Identifiers  
+- All Arithmatic Operators  
+- The Following Separators: 
+    - `()` and `;`  
+- The Following Keywords: 
+    - `while`  
+  
+# Output Format: 
+The lexer outputs a two-column list that shows the token type and lexeme. This two-column list takes the following format:  
+1. The first row shows the format of the output: `TokenType | Lexeme`  
+2. The second row is blank 
+3. The remaining rows represent the input code in the format of the first row.  
+    - (ex: `KEYWORD | while`)  
